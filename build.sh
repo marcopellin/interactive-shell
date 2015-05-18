e2ba800b237c53222867457235d4113cbf12455a
drkraken <kuchuklehjs@gmail.com>
2015-05-19 01:20:45 +0300

    cleanup

--- build.sh
+++
@@ -1,3 +0,0 @@
-cat src/lib/palette.sh src/lib/helpers.sh src/lib/stat.sh src/lib/shell.sh > interactive.sh;
-cd test/temp/;
-bash ../../interactive.sh
--- src/main.sh
+++
@@ -1,6 +0,0 @@
-# import all
-
-. lib/palette.sh
-. lib/helpers.sh
-. lib/stat.sh
-. lib/shell.sh
